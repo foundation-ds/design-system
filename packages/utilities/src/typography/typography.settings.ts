96c0aa40488af13de0669fb90d416e990b62db3a
Tom Newton <tom.newton@schibsted.com>
2021-11-30 10:25:04 +0100

    chore: remove assets

--- packages/utilities/src/typography/typography.settings.ts
+++ packages/utilities/src/typography/typography.settings.ts
@@ -8,18 +8,6 @@ import { calculateRem } from "../utils/utils.converters";
 import { DefaultTheme } from "@foundation-ds/tokens";
 
 export const GlobalFonts = createGlobalStyle`
-  @font-face {
-      font-family: ${({ theme }) => ThemeSelectors.getFontRegular(theme)};
-      src: url(${({ theme }) =>
-        ThemeSelectors.getFontSrc(theme)} + '.svg') format('svg'),
-          url(${({ theme }) =>
-            ThemeSelectors.getFontSrc(theme)} + '.ttf') format('truetype'),
-          url(${({ theme }) =>
-            ThemeSelectors.getFontSrc(theme)} + '.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-    }
-
     body {
       font-family: ${({ theme }) => ThemeSelectors.getFontRegular(theme)};
     }
